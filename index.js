227b11015c7165b1e5e0b0bdbd1af953f414be0b
asucarlos <kuwahara.mee@gmail.com>
2019-08-09 14:29:45 -0400

    find correct URL and clicks

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ const google = "https://www.google.com/"
 const inputContent = "datatables"
 const searchBar = ".gLFyf"
 const searchButton = ".gNO89b"
-const resultElement = "h3.r > a"
+const resultElements = ".r > a"
+const correctURL = "https://datatables.net/"
+
 
 nightmare
-  //go to google, type "datatables" and search
+  //opens google, type "datatables" and search
   .goto(google)
   .type(searchBar, inputContent)
   .click(searchButton)
-  .wait(resultElement)
-  .evaluate(() => {
-    return 'working'
-  })
+  .wait(resultElements)
+  //finds the link that matches to the correct URL and clicks
+  .evaluate((resultElements, correctURL) => {
+    const searchResult = Array.from(document.querySelectorAll(resultElements)).filter(a => a.href === correctURL)
+    searchResult[0].click()
+  }, resultElements, correctURL) //passes parameters from Node scope to browser scope
+  .wait(2000)
   .end()
   .then((result) => {
     console.log(result)
+    // console.log(result[0])
   })
   .catch(error => {
     console.error("Search failed:", error)
