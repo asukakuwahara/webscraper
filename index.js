d93aa8cc781586f531cf618582df29bfd5dbe52f
asucarlos <kuwahara.mee@gmail.com>
2019-08-09 15:07:59 -0400

    output csv file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Nightmare = require("nightmare")
 const nightmare = Nightmare({ show: true })
+const fs = require("fs")
+const { Parser } = require("json2csv");
 
 //list of inputs
 const google = "https://www.google.com/"
@@ -26,14 +28,16 @@ nightmare
     searchResult[0].click()
   }, resultElements, correctURL) //passes parameters from Node scope to browser scope
   .wait(data)
-  .select(lengthSelector, length) //show 100 entries on screen
+  .select(lengthSelector, length) //show all entries on screen
   .wait(data)
   .evaluate((header, data)=>{
     let table = [];
     let fields = [];
+    //store all header to the fields array
     $(header).each((index, value) => {
       fields.push(value.innerHTML)
     })
+    //store all data to the table array
     $(data).each((index, value) => {
       const item = {}
       $(value).children().each((index, value) => {
@@ -41,12 +45,21 @@ nightmare
       })
       table.push(item)
     })
+    //store them in an object
     const outputData = {table, fields}
     return outputData
   }, header, data)
   .end()
   .then((result) => {
-    console.log(result)
+    //parse arrays to csv
+    const fields = result.fields
+    const json2csvParser = new Parser({ fields });
+    const csv = json2csvParser.parse(result.table);
+    //produce a csv file and save in the same folder
+    fs.writeFileSync("output.csv", csv, (err)=>{
+      if(err) throw err;
+      console.log("done!")
+    })
   })
   .catch(error => {
     console.error("Search failed:", error)
